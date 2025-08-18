54b145250b19faa6be8a0517d91537f0c9ef265c
Public Access <dev.katiuskahalaburda@outlook.com>
2025-08-18 19:38:56 -0300

    Debugging app.js

--- js/dashboard/app.js
+++ js/dashboard/app.js
@@ -7,7 +7,7 @@
           logoutBtn.addEventListener("click", async () => {
             try {
               await firebase.auth().signOut();
-              window.location.href = "index.html"; // redirigir al login
+              window.location.href = "./index.html"; // redirigir al login
             } catch (error) {
               console.error("Error al cerrar sesión:", error);
               alert("No se pudo cerrar sesión. Intenta de nuevo.");
